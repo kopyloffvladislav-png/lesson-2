17fa7ae55a1c605ed8ead026977a46047d52c82a
Kopylov Vlad <kopyloff.vladislav@gmail.com>
2026-04-20 00:31:11 +0300

    added new tests

---
+++ src/Test/java/test/me/TestForm.java
@@ -0,0 +1,51 @@
+package test.me;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestForm {
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "Chrome";
+        Configuration.browserVersion = "114.0.5735.90";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+void checkForm() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Влад");
+        $("#lastName").setValue("Копылов");
+        $("#userEmail").setValue("kopyloff.vladislav@gmail.com");
+        $("#gender-radio-3").click();
+        $("#userNumber").setValue("9121093456");
+        $(".react-datepicker-wrapper").click();
+        $(".react-datepicker__year-select").click();
+        $$("option").findBy(text("2000")).click();
+        $(".react-datepicker__month-select").click();
+        $$("option").findBy(text("September")).click();
+        $(".react-datepicker__day--012").click();
+        $("#subjectsInput").setValue("Arts").sendKeys(Keys.ENTER);
+        $("#hobbies-checkbox-2").click();
+        $("#currentAddress").setValue("СПб");
+        $("#react-select-3-input").setValue("NCR").sendKeys(Keys.ENTER);
+        $("#react-select-4-input").setValue("Delhi").sendKeys(Keys.ENTER);
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $$("tr").findBy(text("Student Name")).shouldHave(text("Влад Копылов"));
+        $$("tr").findBy(text("Student Email")).shouldHave(text("kopyloff.vladislav@gmail.com"));
+        $$("tr").findBy(text("Gender")).shouldHave(text("Other"));
+        $$("tr").findBy(text("Mobile")).shouldHave(text("9121093456"));
+        $$("tr").findBy(text("Date of Birth")).shouldHave(text("12 September,2000"));
+        $$("tr").findBy(text("Subjects")).shouldHave(text("Arts"));
+        $$("tr").findBy(text("Hobbies")).shouldHave(text("Reading"));
+        $$("tr").findBy(text("Address")).shouldHave(text("СПб"));
+        $$("tr").findBy(text("State and City")).shouldHave(text("NCR Delhi"));
+    }
