c2136fb25e4f8d3f55702f9ee2025b816b347be3
Kopylov Vlad <kopyloff.vladislav@gmail.com>
2026-04-22 00:00:35 +0300

    added new test

--- src/Test/java/lesson3/SelenideRepository.java
+++ src/Test/java/lesson3/SelenideRepository.java
@@ -1,7 +1,6 @@
 package lesson3;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
--- src/Test/java/test/me/TestBox.java
+++ src/Test/java/test/me/TestBox.java
@@ -1,7 +1,6 @@
 package test.me;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
